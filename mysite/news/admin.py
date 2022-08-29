f1a71d24f00652e6c5e432a2ee262a05f30df424
dpapazyan <dpapazyan@gmail.com>
2022-08-29 19:03:33 +0300

    Second commit

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,16 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import News
+from .models import News, Category
 
 admin.site.register(News)
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'creared_at', 'udpated_at', 'is_published']
+    list_display = ('id', 'title', 'category', 'creared_at', 'udpated_at', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'category')
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
 
 admin.site.unregister(News)
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
+
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -9,8 +9,24 @@ class News(models.Model):
     udpated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.FileField(upload_to='photos/%Y/%m/%d', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
         ordering = ['-creared_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории    '
+        ordering = ['title']
